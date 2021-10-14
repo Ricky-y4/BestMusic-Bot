3942a436ea0a400d5eaca16cafda5f63fc4eb6fe
Ricky-y4 <riccardonale2121@gmail.com>
2021-10-14 20:53:58 +0200

    Add files via upload

---
+++ discord.js
@@ -0,0 +1,68 @@
+const Discord = require("discord.js");
+const settings = require(`./config/settings.json`);
+const filters = require(`./config/filters.json`);
+
+const Enmap = require("enmap");
+const libsodium = require("libsodium-wrappers");
+const ffmpeg = require("ffmpeg-static");
+const voice = require("@discordjs/voice");
+const https = require('https-proxy-agent');
+const client = new Discord.Client({
+    //fetchAllMembers: false,
+    //restTimeOffset: 0,
+    //restWsBridgetimeout: 100,
+    shards: "auto",
+    //shardCount: 5,
+    allowedMentions: {
+      parse: [ ],
+      repliedUser: false,
+    },
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+    intents: [ 
+        Discord.Intents.FLAGS.GUILDS,
+        //Discord.Intents.FLAGS.GUILD_MEMBERS,
+        Discord.Intents.FLAGS.GUILD_MESSAGES,
+        Discord.Intents.FLAGS.GUILD_VOICE_STATES,
+        //Discord.Intents.FLAGS.GUILD_BANS,
+        //Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+        //Discord.Intents.FLAGS.GUILD_INTEGRATIONS,
+        //Discord.Intents.FLAGS.GUILD_WEBHOOKS,
+        //Discord.Intents.FLAGS.GUILD_INVITES,
+        //Discord.Intents.FLAGS.GUILD_PRESENCES,
+        //Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        //Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING,
+        //Discord.Intents.FLAGS.DIRECT_MESSAGES,
+        //Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+        //Discord.Intents.FLAGS.DIRECT_MESSAGE_TYPING
+    ],
+    presence: {
+      activity: {
+        name: `+help | musicium.eu`, 
+        type: "PLAYING", 
+      },
+      status: "online"
+    }
+});
+//const proxy = 'http://123.123.123.123:8080';
+//const agent = https(proxy);
+//Define some Global Collections
+client.commands = new Discord.Collection();
+client.cooldowns = new Discord.Collection();
+client.slashCommands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+client.categories = require("fs").readdirSync(`./src/commands`);
+
+client.setMaxListeners(100); require('events').defaultMaxListeners = 100;
+
+client.settings = new Enmap({ name: "settings",dataDir: "./databases/settings"});
+client.infos = new Enmap({ name: "infos", dataDir: "./databases/infos"});
+
+
+//Require the Handlers                  Add the antiCrash file too, if its enabled
+["events", "commands", "slashCommands", settings.antiCrash ? "antiCrash" : null, "distubeEvent"]
+    .filter(Boolean)
+    .forEach(h => {
+        require(`./handlers/${h}`)(client);
+    })
+//Start the Bot
+client.login(process.env.SECRET_DISCORD_TOKEN)
